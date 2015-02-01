5453c74c6ade8880221a7fe0f6d85dfb0d2eb9e9
Vi Grey <development@pariahvi.com>
2015-01-31 22:27:58 -0600

    added --test, --LAN, --output flag features

--- scripts/ignis.py
+++ scripts/ignis.py
@@ -25,12 +25,54 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
+import glob
+import os
+import http.server
+import re
+import socketserver
 import sys
-from os import listdir
-from os.path import isfile, join
 
-VERSION = '1.0.0.0'
-AUTHOR = 'Vi Grey (http://pariahvi.com)'
+VERSION = "1.0.0.0"
+AUTHOR = "Vi Grey (http://pariahvi.com)"
+HOST = "127.0.0.1"
+PORT = 9999
+
+
+class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        pass
+
+
+class MyTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
+
+
+def get_files(path):
+    file_list = []
+    for root, dirs, files in os.walk(path):
+        if root[-1] == "/":
+            root = root[: -1]
+        if re.search("(/\.)", root) is None:
+            for f in files:
+                file_list.append(root + "/" + f)
+    return file_list
+
+
+# Starts a HTTP server at HOST:PORT with the root directory being path
+def handle_http(path):
+    global HOST
+    os.chdir(path)
+    server = socketserver.TCPServer((HOST, PORT), HTTPRequestHandler)
+    message = ""
+    if HOST == "0.0.0.0":
+        message = " (Open to LAN)"
+    print("Test server at http://localhost:" + str(PORT) + message +
+          "\nPress Ctrl-C to stop server")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.server_close()
+        print("")
 
 
 def help_print():
@@ -42,16 +84,17 @@ def help_print():
           "  -t, --template <file>    Template HTML file\n\n" +
           " Options:\n"
           "  -h, --help               Print Help (this message) and exit\n" +
-          "      --test               Run test web server on port 9999 " +
+          "  -L, --LAN                Open test server up to local network\n"
+          "  -T, --test               Run test web server on port 9999 " +
           "after build\n"
-          "  -v, --verbose            Print verbose messages while " +
-          "processing\n"
-          "      --version            Print version information and exit\n\n" +
+          "  -V, --verbose            Print verbose messages while " +
+          "building\n"
+          "  -v, --version            Print version information and exit\n\n" +
           " Examples:\n" +
           "  ignis -t template.html -i content/text -o example-site\n" +
-          "  ignis -t template/template.html -o example-site\n" +
           "  ignis --verbose -t template/template.html -o example-site\n" +
-          "  ignis -v --test -t template/template.html -o example-site")
+          "  ignis -V -T -L -t template/template.html -o example-site\n" +
+          "  ignis -VTL -t template/template.html -o example-site")
 
 
 def version_print():
@@ -59,35 +102,118 @@ def version_print():
 
 
 def main():
+    global HOST
     help_flag = False
     version_flag = False
-    for arg in sys.argv[1:]:
-        if len(arg) > 2 and arg.find("--") == 0:
-            if arg[2:] == "help":
+    template_flag = False
+    output_flag = False
+    http_flag = False
+    lan_flag = False
+    input_path = "."
+    output_path = ""
+    args = sys.argv[1:]
+    x = 0
+    while x < len(sys.argv[1:]):
+        if len(args[x]) > 2 and args[x].find("--") == 0:
+            if args[x][2:] == "help":
                 help_flag = True
-            elif arg[2:] == "version":
+            elif args[x][2:] == "version":
                 version_flag = True
-            elif arg[2:] != "":
-                print("ignis: unrecognized option " + arg + "\nTry 'ignis " +
-                      "--help' for more information.")
-                sys.exit(1)
-        elif len(arg) > 1 and arg[0] == "-" and arg.find("--") != 0:
-            for x in arg[1:]:
-                if x == "h":
-                    help_flag = True
+            elif args[x][2:] == "test":
+                http_flag = True
+            elif args[x][2:] == "LAN":
+                lan_flag = True
+            elif args[x][2:] == "input":
+                if len(args) > x + 1:
+                    input_path = args[x + 1]
+                    x += 1
+                    break
                 else:
-                    print("ignis: unrecognized option " + arg + "\nTry " +
+                    print("ignis: no input path included\nTry 'ignis " +
+                          "--help' for more information.")
+                    sys.exit(2)
+            elif args[x][2:] == "output":
+                if len(args) > x + 1:
+                    output_flag = True
+                    output_path = args[x + 1]
+                    x += 1
+                    break
+                else:
+                    print("ignis: no output path included\nTry 'ignis " +
+                          "--help' for more information.")
+                    sys.exit(2)
+            elif args[x][2:] != "":
+                print("ignis: unrecognized option " + args[x] + "\nTry " +
+                      "'ignis --help' for more information.")
+                sys.exit(1)
+        elif (len(args[x]) > 1 and args[x][0] == "-" and
+              args[x].find("--") != 0):
+            for y in args[x][1:]:
+                if y == "h":
+                    help_flag = True
+                elif y == "v":
+                    version_flag = True
+                elif y == "T":
+                    http_flag = True
+                elif y == "L":
+                    lan_flag = True
+                elif len(y) == 1 and y == "i":
+                    if len(args) > x + 1:
+                        input_path = args[x + 1]
+                        x += 1
+                        break
+                    else:
+                        print("ignis: no input path included\nTry 'ignis " +
+                              "--help' for more information.")
+                        sys.exit(2)
+                elif len(y) == 1 and y == "o":
+                    if len(args) > x + 1:
+                        output_flag = True
+                        output_path = args[x + 1]
+                        x += 1
+                        break
+                    else:
+                        print("ignis: no path path included\nTry 'ignis " +
+                              "--help' for more information.")
+                        sys.exit(2)
+                else:
+                    print("ignis: unrecognized option " + args[x] + "\nTry " +
                           "'ignis --help' for more information.")
                     sys.exit(1)
-        elif "--".find(arg) != 0:
-            print("ignis: invalid option -- '" + arg + "'\nTry 'ignis " +
+        elif ("--".find(args[x]) == 0 and x != 0) or "--".find(args[x]) != 0:
+            print("ignis: invalid option -- '" + args[x] + "'\nTry 'ignis " +
                   "--help' for more information.")
             sys.exit(1)
-    if help_flag or version_flag:
-        if help_flag:
-            help_print()
+        x += 1
+    if help_flag:
+        help_print()
+    elif version_flag:
+        version_print()
+    if output_flag:
+        output_path = glob.glob(output_path)
+        if len(output_path) == 1:
+            output_path = output_path[0]
         else:
-            version_print()
+            print("ignis: too many output path posibilities")
+    else:
+        print("ignis: output path is required\nTry 'ignis " +
+              "--help' for more information.")
+        sys.exit(5)
+    input_path = glob.glob(input_path)
+    if len(input_path) == 1:
+        input_path = input_path[0]
+        print(get_files(input_path))
+    else:
+        print("ignis: too many input path posibilities")
+        sys.exit(4)
+    if http_flag:
+        if lan_flag:
+            HOST = "0.0.0.0"
+        handle_http(output_path)
+    else:
+        if lan_flag:
+            print("ignis: --test must be included to use LAN flag\n" +
+                  "Try 'ignis --help' for more information.")
 
 if __name__ == "__main__":
     main()
